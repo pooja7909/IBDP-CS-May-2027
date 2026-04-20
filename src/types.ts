54ad09704f98c52b7b3e22f0649d61d24107fb8c
pooja7909 <52460015+pooja7909@users.noreply.github.com>
2026-04-20 14:53:56 +0200

    Add files via upload

---
+++ src/types.ts
@@ -0,0 +1,62 @@
+
+export enum Section {
+  // Course Guide
+  ROADMAP = 'roadmap',
+  TIMELINE = 'timeline',
+  EXAMS = 'exams',
+  IA_GUIDE = 'ia_guide',
+  
+  // Topic A1: Computer Fundamentals
+  A1_HARDWARE = 'a1_hardware',
+  A1_OS = 'a1_os',
+  
+  // Topic A2: Networks
+  A2_BASICS = 'a2_basics',
+  A2_SECURITY = 'a2_security',
+
+  // Topic A3: Databases
+  A3_HOME = 'a3_home',
+  A3_SCHEMAS = 'a3_schemas',
+  A3_NORMALIZATION = 'a3_normalization',
+  A3_SQL_BASICS = 'a3_sql_basics',
+  A3_SQL_QUERIES = 'a3_sql_queries',
+  A3_SQL_LAB = 'a3_sql_lab',
+  A3_PLAYGROUND = 'a3_playground',
+  A3_ACID_VIEWS = 'a3_acid_views',
+
+  // Topic A4: Machine Learning (NEW)
+  A4_ML_TYPES = 'a4_ml_types',
+  A4_NEURAL_NETS = 'a4_neural_nets',
+  A4_ETHICS = 'a4_ethics',
+
+  // Topic B1: Computational Thinking
+  B1_SPEC = 'b1_spec',
+  B1_CONCEPTS = 'b1_concepts',
+  B1_APPS = 'b1_apps',
+  B1_FLOWCHARTS = 'b1_flowcharts',
+  B1_FLOWCHART_LAB = 'b1_flowchart_lab',
+  B1_PRACTICE = 'b1_practice',
+
+  // Topic B2: Programming
+  B2_BASICS = 'b2_basics',
+  B2_STRUCTURES = 'b2_structures',
+  B2_ALGORITHMS = 'b2_algorithms',
+  B2_RECURSION = 'b2_recursion',
+  B2_FILES_EXCEPTIONS = 'b2_files_exceptions',
+  B2_PYTHON_LAB = 'b2_python_lab',
+  B2_PRACTICE = 'b2_practice',
+
+  // Topic B3: Object-Oriented Programming
+  B3_CONCEPTS = 'b3_concepts',
+  B3_CLASS_DESIGN = 'b3_class_design',
+  B3_INHERITANCE = 'b3_inheritance',
+  B3_HL_ADVANCED = 'b3_hl_advanced',
+  B3_TRACE_LAB = 'b3_trace_lab',
+  B3_PRACTICE = 'b3_practice',
+
+  // Topic B4: Abstract Data Types (NEW HL ONLY)
+  B4_HL_ADT = 'b4_hl_adt',
+
+  // Review
+  PRACTICE = 'practice',
+}
